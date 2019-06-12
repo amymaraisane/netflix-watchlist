548a7d2df6d4bfad67da5ae58245cbcb31cfa790
amymaraisane <amy.maraisane@gmail.com>
2019-06-12 12:07:08 -0400

    put and delete routes added

--- api/app.js
+++ api/app.js
@@ -1,17 +1,21 @@
 const express = require('express');
-        app = express();
-        PORT = process.env.PORT || 3000;
+      app = express();
+      PORT = process.env.PORT || 3000;
+      bodyParser = require('body-parser');
 
-        contentRoutes = require('./routes/content')
+      contentRoutes = require('./routes/content');
+
 
 app.use(express.static(__dirname + '/views'));
-
+app.use(bodyParser.urlencoded({ extended: true }));;
+app.use(bodyParser.json());
 
 app.get("/", (req, res)=>{
     res.sendFile("index.html");
 });
 
 app.use('/api/content', contentRoutes);
+
 
 app.listen(PORT, () => {
     console.log(`Our app is running on port ${ PORT }`);

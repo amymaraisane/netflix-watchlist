97d81d058c52439114fb4326b3029883bdf7cf29
amymaraisane <amy.maraisane@gmail.com>
2019-06-12 18:29:23 -0400

    decrease indents, add explicit key:variable lookup

--- api/routes/content.js
+++ api/routes/content.js
@@ -2,7 +2,7 @@ var express = require ('express');
     router  = express.Router();
     db = require('../models');
 
-//GET all data from db using .find method
+//GET all data from db
 router.get('/', (req, res)=>{
     db.Content.find()
     .then(todos=>{
@@ -16,47 +16,53 @@ router.get('/', (req, res)=>{
 //POST new data to the db
 router.post('/', (req, res)=>{
     db.Content.create(req.body)
-        .then(newContent=>{
-            res.json(newContent);
-        })
-        .catch(err=>{
-            res.send(err);
-        })
+    .then(newContent=>{
+        res.json(newContent);
+    })
+    .catch(err=>{
+        res.send(err);
+    })
 });
+
+module.exports = router;
+
 
 //GET one data item from db
 router.get('/:contentid', (req, res)=>{
     db.Content.findById(req.params.contentid)
-        .then(foundContent=>{
-            res.json(foundContent);
-        })
-        .catch(err=>{
-            res.send(err);
-        })
+    .then(foundContent=>{
+        res.json(foundContent);
+    })
+    .catch(err=>{
+        res.send(err);
+    })
 });
 
 //PUT an update onto one data item
 router.put('/:contentid', (req, res)=>{
-    db.Content.findByIdAndUpdate(req.params.contentid, req.body, {new: true})
-        .then(updatedContent=>{
-            res.json(updatedContent);
-        })
-        .catch(err=>{
-            res.send(err);
-        })
+    db.Content.findOneAndUpdate({_id: req.params.contentid}, req.body, {new: true})
+    .then(updatedContent=>{
+        res.json(updatedContent);
+    })
+    .catch(err=>{
+        res.send(err);
+    })
 });
 
 //DELETE one data item
 router.delete('/:contentid', (req, res)=>{
-    db.Content.findByIdAndDelete(req.params.contentid)
-        .then(()=>{
-            res.json({message: "Content Deleted"});
-        })
-        .catch(err=>{
-            res.send(err);
-        })
+    db.Content.findOneAndRemove({_id: req.params.contentid})
+    .then(()=>{
+        res.json({message: "Content Deleted"});
+    })
+    .catch(err=>{
+        res.send(err);
+    })
 });
 
 module.exports = router;
+
+
+
 
 

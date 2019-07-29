300202940e95290adb3326d546d77463fed93947
amymaraisane <amy.maraisane@gmail.com>
2019-07-29 14:32:02 -0400

    correct ev

--- api/models/index.js
+++ api/models/index.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 mongoose.set('debug', true);
 
-const URI = 'mongodb://localhost/watchlist || DATABASEURL'
+const URI = 'mongodb://localhost/watchlist' || DATABASEURL
 
 mongoose.connect(URI, {
   useNewUrlParser: true,

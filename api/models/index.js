0699002fbb4bda2798bdf563362f3e11560e81e5
amymaraisane <amy.maraisane@gmail.com>
2019-08-18 18:41:22 -0400

    switched order with local first

--- api/models/index.js
+++ api/models/index.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 mongoose.set('debug', true);
 
-const URI = DATABASEURL
+const URI = 'mongodb://localhost/watchlist' || DATABASEURL
 
 
 mongoose.connect(URI, {

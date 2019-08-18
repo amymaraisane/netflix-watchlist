bf9ed4d2f4e6e1bf937fcafe85cc7d1b6962d128
amymaraisane <amy.maraisane@gmail.com>
2019-08-18 19:16:53 -0400

    add process.env.DATABASEURL

--- api/models/index.js
+++ api/models/index.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 mongoose.set('debug', true);
 
-const URI = DATABASEURL
+const URI = process.env.DATABASEURL
 
 mongoose.connect(URI, {
   useNewUrlParser: true,
